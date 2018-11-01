e671da962566e4d80ec59f6f421facd88b27d29a
rattanchen <1005332621@qq.com>
2018-11-01 16:15:39 +0800

    [add] 添加项目

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="center">Hello Nuxt !</h1>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style scoped>
+h1.center{
+  text-align: center;
+  margin-top: 50%;
+}
+</style>
